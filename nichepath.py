55099ae685391daecbfa270494b20cb71cf176bd
Adam Glenn <12aglenn34@gmail.com>
2016-09-22 17:57:43 -0400

    Update nichepath.py

--- nichepath.py
+++ nichepath.py
@@ -7,6 +7,9 @@ Created on Mon Sep 19 14:18:30 2016
 
 import random
 
+#Dictionary of possible node connections, running the program creates a random node path, or niche
+#Dictionary will need updating to account for new possible nodes
+
 T = {
     "mine" : {"conversion", "enrichment", "hwr"},
     "conversion" : {"enrichment", "uo2 fabrication", "uo2 fabrication", "uo2 fabrication", "triso fabrication", "hwr"}, 
@@ -20,19 +23,21 @@ T = {
     "rbmk" : {"storage", "dry reprocessing","purex","near surface repository", "deep geological repository"},         
     "pb" : {"near surface repository", "deep geological repository"},         
     "storage" : {"dry reprocessing","purex","near surface repository", "deep geological repository"},         
-    "dry reprocessing" : {"conversion","enrichment","hwr", "mox"},
+    "dry reprocessing" : {"conversion","enrichment", "hwr", "mox"},
     "purex" : {"conversion", "enrichment", "hwr", "mox"},
     "mox fabrication" : {"fr", "lwr", "htgr", "rbmk"},
     "deep geological repository" : {None},          
-    "near surface repository" : {None} #need to add a null
-}
+    "near surface repository" : {None} 
+    }
 
-def test(niches, nichelist = {"mine"}, choice = "mine"):
-     if niches == 1 or None in nichelist:
-         return nichelist
-     else:
-         choice = random.sample(T[choice],1)[0]
-         nichelist.add(choice)
-         return test(niches-1, nichelist, choice)
-
+#randomniche generates a set of possible nodes along a path of niches
+#to change start point 
+def randomniche(niches, choice = "mine"):
+    nichelist = {choice}
+    if niches == 1 or None in nichelist:
+        return nichelist
+    else:
+        choice = random.sample(T[choice],1)[0]
+        nichelist.add(choice)
+        return randomniche(niches-1, choice)
     
